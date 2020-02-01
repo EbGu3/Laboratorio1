c6e70973a7cb82c83e365328013a9ee3b47a75ad
AlexUwU <alexander.2014022@gmail.com>
2020-02-01 10:50:24 -0600

    Inicio del arbol

---
+++ Laboratorio1/Controllers/ArbolB.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratorio1.Controllers
+{
+    public class ArbolB
+    {
+        internal int PosicionAproxNodo(int key )
+        {
+            int pos = key.Count; 
+            for (i = 0; i< key.Count; i++)
+            {
+                if ((key[i]> key) || (key[1] == Utilidades.ApuntadorVac))
+                {
+                    pos = i;
+                    break;
+                }
+            }
+            return pos;
+        }
+    }
+}
